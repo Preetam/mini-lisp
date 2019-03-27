7a73799e6acac3744e3aea5ed3df1adcfdb38fad
Preetam Jinka <pj@preet.am>
2019-03-26 22:08:55 -0700

    basic car, cdr, cons

--- env.go
+++ env.go
@@ -17,6 +17,20 @@ func DefaultEnv() *Environment {
 	env.Set(">=", &Procedure{f: func(args []Expression) (Expression, int) { return Bool(args[0].(Number) >= args[1].(Number)), 0 }})
 	env.Set("=", &Procedure{f: func(args []Expression) (Expression, int) { return Bool(args[0].(Number) == args[1].(Number)), 0 }})
 	env.Set("list", &Procedure{f: func(args []Expression) (Expression, int) { return List(args), 0 }})
+	env.Set("car", &Procedure{f: func(args []Expression) (Expression, int) { return args[0].(List)[0], 0 }})
+	env.Set("cdr", &Procedure{f: func(args []Expression) (Expression, int) { return args[0].(List)[1:], 0 }})
+	env.Set("cons", &Procedure{f: func(args []Expression) (Expression, int) {
+		if len(args) == 1 {
+			return List{args[0]}, 0
+		}
+		if args[1] == nil || IsNil(args[1]) {
+			return List{args[0]}, 0
+		}
+		if _, ok := args[1].(List); ok {
+			return append(List{args[0]}, args[1].(List)...), 0
+		}
+		return List{args[0], args[1]}, 0
+	}})
 	env.Set("print", &Procedure{f: func(args []Expression) (Expression, int) { fmt.Println(args[0].(String)); return Nil{}, 0 }})
 	env.Set("str", &Procedure{f: func(args []Expression) (Expression, int) { return String(args[0].ExprToStr()), 0 }})
 	env.Set("save", &Procedure{f: func(args []Expression) (Expression, int) {
